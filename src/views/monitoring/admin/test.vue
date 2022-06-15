ef21cbfaa6c251071d3d73228f900a57cb096256
aughusbueno <aughusf089@gmail.com>
2022-06-16 01:06:26 +0800

    barshit

---
+++ src/views/monitoring/admin/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container py-5">
+    <BarShit/>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import auth from "../../../utils/authHeader";
+import BarShit from '../../../components/bar.shit.vue'
+
+export default {
+  name: "admin-monitoring-test",
+  components: {
+    BarShit
+  },
+  data() {
+    return {
+      list: [],
+    };
+  },
+  mounted() {
+    auth("admin");
+  },
+  methods: {},
+};
+</script>
