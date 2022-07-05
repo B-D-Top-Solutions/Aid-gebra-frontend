2230d9da43c4cb0899dd5f1e76fd552b2e90f34e
weishengit <weishenemail@gmail.com>
2022-07-05 18:32:44 +0800

    prepend '09' to contact number

--- src/views/monitoring/monitor/allStudents.vue
+++ src/views/monitoring/monitor/allStudents.vue
@@ -49,7 +49,7 @@
                 <small>{{ e.email }}</small>
               </td>
               <td>
-                <small>{{ e.contact }}</small>
+                <small>{{ `09${e.contact}` }}</small>
               </td>
               <td>
                 <span
